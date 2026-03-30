9f3ee807b20a0aa01f20e3a5ac1454b98be43ad1
Richard Valenzuela <valenzuelasedanojhon@gmail.com>
2026-03-30 17:44:27 -0500

    Fix CORS Vercel

--- src/main/java/com/example/demo/CorsConfig.java
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -16,8 +16,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-                "http://localhost:4200",
-                "https://aris-system-vortex-88-829iiw7gv.vercel.app"
+                "*"
         ));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
